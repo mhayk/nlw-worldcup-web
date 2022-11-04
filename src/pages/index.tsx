29b57615b0458115ce8e3ed5806bfe1b1607a931
Mhayk Whandson <hi@mhayk.com>
2022-11-04 15:51:17 +0000

    feat: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,38 +6,52 @@ import iconCheckImg from '../assets/icon-check.svg'
 
 export default function Home() {
   return (
-    <div>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28'>
       <main>
         <Image src={logoImg} alt="NLW World Cup" />
 
-        <h1>Create your own cup pool and share it with friends!</h1>
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+          Create your own cup pool and share it with friends!
+        </h1>
 
-        <div>
+        <div className='mt-10 flex items-center gap-2'>
           <Image src={usersAvataresExampleImg} alt="" />
 
-          <strong>
-            <span>+12.592 people is alredy using</span>
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>+12.592</span> people is alredy using
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder="What is the name of your pool ?" />
-          <button type="submit">Create your pool</button>
+        <form className='mt-10 flex gap-2'>
+          <input
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            type="text"
+            required
+            placeholder="What is the name of your pool ?"
+          />
+          <button
+            className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
+            type="submit"
+          >
+            Create my pool
+          </button>
         </form>
 
-        <p>After creating your pool, you will receive a unique code that you can use to invite others 🚀</p>
+        <p className='text-gray-300 mt-4 text-sm leading-relaxed'>
+          After creating your pool, you will receive a unique code that you can use to invite others 🚀
+        </p>
 
-        <div>
-          <div>
+        <div className='mt-10 pt-10 border-t border-gray-600 divide-x grid grid-cols-2 text-gray-100'>
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt="" />
-            <div>
+            <div className=''>
               <span>+2.034</span>
               <span>Pools created</span>
             </div>
           </div>
-          <div>
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt="" />
-            <div>
+            <div className=''>
               <span>+192.847</span>
               <span>Guesses sent</span>
             </div>
